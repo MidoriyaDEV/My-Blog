8f51c83786b6ae94ac4728f9f4d6361e446d58ec
Victor Nascimento de Melo <victornascimentobr48@gmail.com>
2024-11-06 17:53:55 -0300

    Delete My-Blog-master directory

--- My-Blog-master/javascript/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('#mobile_btn').on('click' , function () {
-        $('mobile_menu').toggLeClass('active');
-        $('mobile_btn').find('i').toggLeClass('fa-x');
-    });
-});
